b6db7c56f574fe54da1c243ed112d06249118904
Your Name <you@example.com>
2023-10-10 17:09:39 +0300

    2 бандита

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите количество цветов");
-        int color = scanner.nextInt();
-        if(color>=1) {
-            int N=color+1;
-            System.out.println("Минимальное число бусин= "+ N);
+        System.out.println("количество банок простреленых Гарри: ");
+        int BG = scanner.nextInt();
+        System.out.println("количество банок простреленых Ларри: ");
+        int BL = scanner.nextInt();
 
-        }
+        int BS=BG+BL;
+        int G1=BS-BG-1;
+        int L1=BS-BL-1;
+            System.out.println("Гарри не прострелил "+G1+" банок, Ларри "+L1+" банок.");
+
     }
 }
